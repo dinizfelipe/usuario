5b20329947974d9679872bc9a3908abb7c66aa22
Felipe Diniz <felipedinizdev@gmail.com>
2026-02-10 15:24:46 -0300

    dockerfile, docker-compose e exceptions

--- src/main/java/com/felipe/usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/felipe/usuario/business/converter/UsuarioConverter.java
@@ -21,8 +21,10 @@ public class UsuarioConverter {
                 .nome(usuarioDTO.getNome())
                 .email(usuarioDTO.getEmail())
                 .senha(usuarioDTO.getSenha())
-                .enderecos(paraListaEndereco(usuarioDTO.getEnderecos()))
-                .telefones(paraListaTelefones(usuarioDTO.getTelefones()))
+                .enderecos(usuarioDTO.getEnderecos() != null ?
+                        paraListaEndereco(usuarioDTO.getEnderecos()): null)
+                .telefones(usuarioDTO.getTelefones() != null?
+                        paraListaTelefones(usuarioDTO.getTelefones()): null)
                 .build();
     }
 
@@ -62,8 +64,10 @@ public class UsuarioConverter {
                 .nome(usuario.getNome())
                 .email(usuario.getEmail())
                 .senha(usuario.getSenha())
-                .enderecos(paraListaEnderecoDTO(usuario.getEnderecos()))
-                .telefones(paraListaTelefonesDTO(usuario.getTelefones()))
+                .enderecos(usuario.getEnderecos() != null ?
+                        paraListaEnderecoDTO(usuario.getEnderecos()): null)
+                .telefones(usuario.getTelefones() != null?
+                        paraListaTelefonesDTO(usuario.getTelefones()): null)
                 .build();
     }
 
